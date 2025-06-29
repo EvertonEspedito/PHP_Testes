5fde4c295b0f8f4d1ab0220bf22dac2ac2ce71fe
Everton Espedito <evertondev505@gmail.com>
2025-06-29 07:42:35 -0300

    Update expressoes.php

--- expressoes.php
+++ expressoes.php
@@ -1,7 +1,25 @@
 <?php 
-    $teste = 'Everton';
+    //$teste = 'Everton';
 
-    preg_match('/(.*?)ton/',$teste,$retorno );
-    echo $retorno[1];
+    //preg_match('/(.*?)ton/',$teste,$retorno );
+    //echo $retorno[1];
+    echo 'validação de CPF:';
+    function cpfValido($cpf){
+        $expressao = '/[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/';#EXPRESSÃ PARA CPF: 000.000,000-00
+        return preg_match($expressao, $cpf);
+    }
+    if (isset($_POST['acao'])) {
+        $cpf = $_POST['CPF'];
+        if (cpfValido($cpf)) {
+            echo' O CPF é válido! ';
+        }else{
+            echo('O CPF não é válido!');
+        }
+    }
 ?> 
-PAREI NO CPF
+
+<form action="" method="post">
+    <input type="text" name="CPF">
+    <input name="acao" type="submit">
+</form>
+
